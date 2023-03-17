a1f99fccd15e4abc94623446b786e8febc23c3b7
Marcelle Dantas <marcelle.dantas@mindsatwork.com.br>
2023-03-16 22:07:19 -0300

    EX0402

---
+++ src/cap4/EX0402.java
@@ -0,0 +1,18 @@
+package cap4;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author imarc
+ * @version 1.0 Created on 16 de mar de 2023
+ */
+public class EX0402
+{
+
+	public static void main( final String[] args )
+	{
+		final Vetor vetor = new Vetor( 3.35, 4.68 );
+		System.out.println( vetor.clone().toString() );
+	}// func
+}
